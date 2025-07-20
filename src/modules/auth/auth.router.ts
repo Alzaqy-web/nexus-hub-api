cb9d0a18d3777442e7da45bcbfe8b240bfeb41db
AL ZAQY <alzaqy19@gmail.com>
2025-07-20 17:21:37 +0700

    fix: resolve merge conflict in auth.router.ts

--- src/modules/auth/auth.router.ts
+++ src/modules/auth/auth.router.ts
@@ -1,11 +1,6 @@
 import { Router } from "express";
 import { AuthController } from "./auth.controller";
-<<<<<<< HEAD
-import { validateOrReject } from "class-validator";
-import { validateBody } from "../../middlewares/validation.middleware";
-=======
 import { validateBody } from "../../middleware/validation.middleware";
->>>>>>> main
 import { LoginDTO } from "./dto/login.dto";
 import { RegisterDTO } from "./dto/register.dto";
 
@@ -16,36 +11,20 @@ export class AuthRouter {
   constructor() {
     this.router = Router();
     this.authController = new AuthController();
-<<<<<<< HEAD
-    this.initialRoutes();
-  }
-
-  private initialRoutes = () => {
-=======
     this.initializeRoutes();
   }
 
   private initializeRoutes = () => {
->>>>>>> main
     this.router.post(
       "/login",
       validateBody(LoginDTO),
       this.authController.login
     );
-<<<<<<< HEAD
-=======
-
->>>>>>> main
     this.router.post(
       "/register",
       validateBody(RegisterDTO),
       this.authController.register
     );
-<<<<<<< HEAD
-  };
-
-  public getRouter = () => {
-=======
 
     this.router.post(
       "/register-eo",
@@ -55,7 +34,6 @@ export class AuthRouter {
   };
 
   getRouter = () => {
->>>>>>> main
     return this.router;
   };
 }
